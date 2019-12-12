8de48bf7c58c4862f95050808bc53f92ffd7145c
ahmedMshaban <ahmed.ms.elias@gmail.com>
2019-12-12 07:12:36 +0300

    Heal player functionlity

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -8,15 +9,7 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode) {
-  let maxDamge;
-  if (mode === 'ATTACK') {
-    maxDamge = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
-    maxDamge = STRONG_ATTACK_VALUE;
-  }
-  const monsterDamge = dealMonsterDamage(STRONG_ATTACK_VALUE);
-  currentMonsterHealth -= monsterDamge;
+function endRound() {
   const playerDamge = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamge;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
@@ -28,6 +21,18 @@ function attackMonster(mode) {
   }
 }
 
+function attackMonster(mode) {
+  let maxDamge;
+  if (mode === 'ATTACK') {
+    maxDamge = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamge = STRONG_ATTACK_VALUE;
+  }
+  const monsterDamge = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= monsterDamge;
+  endRound();
+}
+
 function strongAttackHandler() {
   attackMonster('ATTACK');
 }
@@ -36,5 +41,19 @@ function attackHandler() {
   attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal to more than you max inital health.");
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
