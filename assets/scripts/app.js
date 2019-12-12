224615016ea37738e54271f5185def273f221a38
ahmedMshaban <ahmed.ms.elias@gmail.com>
2019-12-12 06:30:22 +0300

    Strong Attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,8 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const monsterDamge = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamge;
+  if (mode === 'ATTACK') {
+    maxDamge = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamge = STRONG_ATTACK_VALUE;
+  }
+  const monsterDamge = dealMonsterDamage(STRONG_ATTACK_VALUE);
   currentMonsterHealth -= monsterDamge;
   const playerDamge = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamge;
@@ -21,4 +28,13 @@ function attackHandler() {
   }
 }
 
+function strongAttackHandler() {
+  attackMonster('ATTACK');
+}
+
+function attackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
